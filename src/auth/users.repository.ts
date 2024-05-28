c01df142f59c919eb98766d3ee939f5dea5ddd2d
hko <h.klose@dotsource.de>
2024-05-28 08:45:52 +0200

    add password salt & hash

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -6,6 +6,7 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
@@ -14,10 +15,14 @@ export class UsersRepository extends Repository<User> {
   }
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
+
     try {
       await this.save(user);
     } catch (error) {
