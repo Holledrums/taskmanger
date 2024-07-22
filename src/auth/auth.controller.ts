7648f2d991f7d1fc24ea7f28946845e4fee94abb
hko <h.klose@dotsource.de>
2024-07-22 14:15:48 +0200

    add auth module & service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,4 +15,12 @@ export class AuthController {
   ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/signin')
+  signIn(
+    @Body(new JoiValidationPipe(authValidationSchema))
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
